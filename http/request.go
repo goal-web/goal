3fc3404ccde0ef01e9acb51d201ed9c92981e43d
qbhy <96qbhy@gmail.com>
2021-09-19 08:13:22 +0800

    feat: 重写request的get方法

--- http/request.go
+++ http/request.go
@@ -2,8 +2,19 @@ package http
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/qbhy/goal/utils"
 )
 
 type Request struct {
 	echo.Context
+}
+
+func (this Request) Get(key string) (value interface{}) {
+	if value = utils.StringOr(
+		this.Param(key),
+		this.QueryParams().Get(key),
+	); value != "" {
+		return value
+	}
+	return utils.NotNil(this.Context.Get(key), this.FormValue(key))
 }
