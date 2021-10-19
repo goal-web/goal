caa5e35cff74bf6ad935792d5c54162f8f9531cf
qbhy <qbhy0715@qq.com>
2021-10-19 16:21:52 +0800

    test: 添加convert方法的测试代码

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -10,8 +10,16 @@ import (
 func TestMergeFields(t *testing.T) {
 	fields1 := contracts.Fields{"a": "a"}
 	utils.MergeFields(fields1, map[string]interface{}{
-		"a": "b",
+		"a":     "b",
+		"int":   1,
+		"bool":  true,
+		"stringBool":  "(true)",
 	})
 
 	assert.True(t, fields1["a"] == "b")
+	assert.True(t, utils.GetStringField(fields1, "a") == "b")
+	assert.True(t, utils.GetInt64Field(fields1, "int") == 1)
+	assert.True(t, utils.GetBoolField(fields1, "bool"))
+	assert.True(t, utils.GetBoolField(fields1, "stringBool"))
+
 }
