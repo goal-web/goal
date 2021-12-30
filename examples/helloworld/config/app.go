c2456d06e3ae2de659e2196daf433c1cbe2d3762
qbhy <qbhy0715@qq.com>
2021-12-30 10:17:58 +0800

    feat: 优化config目录

--- examples/helloworld/config/app.go
+++ examples/helloworld/config/app.go
@@ -1,1 +1,11 @@
 package config
+
+import "github.com/qbhy/goal/config"
+
+var (
+	configs = make(map[string]config.ConfigProvider)
+)
+
+func Configs() map[string]config.ConfigProvider {
+	return configs
+}
