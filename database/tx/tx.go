02e7d3ae08fb440199218ca52c3222740a7187e8
qbhy <qbhy0715@qq.com>
2022-01-05 18:07:23 +0800

    feat: 添加sqlite驱动，添加事务支持

---
+++ database/tx/tx.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/qbhy/goal/contracts"
+)
+
+type Tx struct {
+	*sqlx.Tx
+}
+
+func (this *Tx) Exec(query string, args ...interface{}) (contracts.Result, error) {
+	return this.Tx.Exec(query, args...)
+}
